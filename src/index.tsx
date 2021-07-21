eb8de92ca13a7375e7fe942f1f39d33759b32d25
kaidohussar <kaido@onoffapp.com>
2021-07-21 22:33:04 +0300

    up

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-const Application: React.FC<{}> = () => <h1>Appication</h1>;
-
-render(<Application />, document.getElementById("root"));
