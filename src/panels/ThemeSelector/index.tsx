35b87195812b0b7312125ce66b78ef226f128b32
kaidohussar <kaido@onoffapp.com>
2021-07-27 22:45:22 +0300

    up

--- src/panels/ThemeSelector/index.tsx
+++ src/panels/ThemeSelector/index.tsx
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-export const ThemeSelector = ({api}: any) => {
-   const [selectedTheme, setSelectedTheme] = useState<string>();
+export const ThemeSelector = ({ api }: any) => {
+  const [selectedTheme, setSelectedTheme] = useState<string>();
 
-   const selectTheme = (theme: string) => {
-      if (api && api.getChannel) {
-         const channel = api.getChannel();
-         channel.emit('selectTheme', theme);
-      }
+  const selectTheme = (theme: string) => {
+    if (api && api.getChannel) {
+      const channel = api.getChannel();
+      channel.emit('selectTheme', theme);
+    }
 
-      setSelectedTheme(theme);
-   };
+    setSelectedTheme(theme);
+  };
 
-   return (
-      <div>
-         <button style={{border: selectedTheme === 'dark' ? '1px solid red' : 'unset'}} onClick={() => selectTheme('dark')}>
-            darks
-         </button>
-         <button style={{border: selectedTheme === 'light' ? '1px solid red' : 'unset'}} onClick={() => selectTheme('light')}>
-            lights
-         </button>
-      </div>
-   );
+  return (
+    <div>
+      <button style={{ border: selectedTheme === 'dark' ? '1px solid red' : 'unset' }} onClick={() => selectTheme('dark')}>
+        darks
+      </button>
+      <button style={{ border: selectedTheme === 'light' ? '1px solid red' : 'unset' }} onClick={() => selectTheme('light')}>
+        lights
+      </button>
+    </div>
+  );
 };
