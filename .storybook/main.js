cb8950eb5c2719610ca7ca33660df18c582c679f
Kaido Hussar <kaidohus@gmail.com>
2021-08-07 15:40:22 +0300

    up

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,14 @@ const path = require('path');
 module.exports = {
    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
    resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+      alias: {
+         src: path.resolve(__dirname, '../src/*'),
+         stories: path.resolve(__dirname, '../src/stories'),
+         components: path.resolve(__dirname, '../src/components'),
+         theming: path.resolve(__dirname, '../src/theming'),
+      },
+      extensions: ['.tsx', '.ts', '.js', '.jsx', '.mdx'],
    },
    addons: [
       '@storybook/addon-links',
